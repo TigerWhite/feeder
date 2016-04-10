a0669ec79fc974c7e7d32ec08e90f97d419bcca5
TigerWhite <tigerwhite.nkz@gmail.com>
2016-04-10 21:16:00 +0700

    test commit 8

--- feeder.php
+++ feeder.php
@@ -33,14 +33,11 @@ class Feeder extends Module
 	
 	public function __construct()
 	{
-
-
-
 		$this->name = 'feeder';
-		$this->tab = 'front_office_features';
-		$this->name = 'feeder';
-		$this->version = '0.7.2';
-		$this->author = 'PrestaShop';
+
+
+
+		
 		$this->need_instance = 0;
 		
 		$this->_directory = dirname(__FILE__).'/../../';
